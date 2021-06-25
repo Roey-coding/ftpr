ed62448dada57701fa42fdc9427e3baa7cf0a716
roey-coding <roey.eshkar@gmail.com>
2021-06-25 22:25:51 +0300

    Added a server, this server is currently a dumy server designed to accept a conncetion so sent packets will show up on wireshark (the -q option is added for quiet mode from the hello message)

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+IP = '0.0.0.0' 
+PORT = 2004
+WELCOME_MESSAGE = """
+Welcome to the server.
+this is a basic welcome message,
+You can turn it off by adding -q as an argument.
+"""
+
+
+def main():
+	tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+	tcp_server.bind((IP, PORT))
+
+	while 1:
+		x = input("start?")
+		
+		if(x == "no") :
+			break
+		
+		tcp_server.listen(4)
+		(conn, (ip,port)) = tcp_server.accept()
+
+def welcome():
+	print(WELCOME_MESSAGE)
+
+if __name__ == "__main__":
+	if(len(sys.argv) == 1 or sys.argv[1] != "-q"):
+		welcome()
+
+	main()
